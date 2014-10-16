baa19abb139fdc82d8c3e19fe200530d3f6f63d1
neslonso <neslonso@gmail.com>
2014-10-16 20:58:19 +0200

    [+] Integración Composer (2)
    [x] PHPUnit requerido vía composer
    [x] Cron-expression requerido vía composer

--- appSite/server/clases/Pages/zzTools/composer/composer.php
+++ appSite/server/clases/Pages/zzTools/composer/composer.php
@@ -38,70 +38,20 @@ class composer extends Home implements IPage {
 		require_once( str_replace("//","/",dirname(__FILE__)."/")."markup/css.php");
 	}
 	public function markup() {
-		/*
-		register_shutdown_function(function () {
-			echo "<pre>";
-			var_dump($GLOBALS['dbg_stack']);
-			echo "</pre>";
-		});
-		register_tick_function(function () {
-			$GLOBALS['dbg_stack'] = debug_backtrace(0);
-		});
-		declare(ticks=1);
-		// Composer\Factory::getHomeDir() method
-		// needs COMPOSER_HOME environment variable set
-		error_log('COMPOSER_HOME=' . SKEL_ROOT_DIR);
-		putenv('COMPOSER_HOME=' . SKEL_ROOT_DIR);
-
-		//$tmpFile = tmpfile();
-		$tmpFile = fopen('./composer.output.txt','w+');
-		$output  = new StreamOutput($tmpFile);
-		// call `composer install` command programmatically
-		$input = new ArrayInput(array('command' => 'install'));
-		$application = new Application();
-		$application->setAutoExit(false); // prevent `$application->run` method from exitting the script
-		$application->setCatchExceptions(false);
-		error_log('0');
-		$application->run($input,$output);
-		error_log('************************************************************************************1');
-
-		fseek($tmpFile, 0);
-		$output = fread($tmpFile, 1024);
-		fclose($tmpFile);
-		echo $output;
-
-		//Probar esta salida
-		//$output = new BufferedOutput();
-		//$input = new ArrayInput($arguments);
-		//$code = $command->run($input, $output);
-		////RUN
-		//if($code == 0) {
-		//	$outputText = $output->fetch();
-		//	echo $outputText;
-		//}
-		*/
-
-		/*
-		echo "<pre>";
-		//echo shell_exec('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar');
-		echo shell_exec('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar install --dry-run');
-		//echo shell_exec('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar install --dry-run --optimize-autoloader');
-		echo "</pre>";
-		*/
 		putenv('COMPOSER_HOME=' . SKEL_ROOT_DIR);
 		$descriptorspec = array(
 			0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
 			1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
 			2 => array("pipe", "w"),  // stderr is a pipe that the child will write to
 		);
-		//$cwd = '/tmp';
-		//$env = array('some_option' => 'aeiou');
 		//$process = proc_open('php', $descriptorspec, $pipes, $cwd, $env);
 		//$process = proc_open('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar', $descriptorspec, $pipes);
 		//$process = proc_open('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar install --dry-run', $descriptorspec, $pipes);
 		//$process = proc_open('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar install', $descriptorspec, $pipes);
-		$process = proc_open('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar install --optimize-autoloader', $descriptorspec, $pipes);
-		//$process = proc_open('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar update', $descriptorspec, $pipes);
+		//$process = proc_open('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar install --optimize-autoloader', $descriptorspec, $pipes);
+		error_log('ante_proc_open');
+		$process = proc_open('php '.SKEL_ROOT_DIR.'includes/server/vendor/composer.phar update', $descriptorspec, $pipes);
+		error_log('tras_proc_open');
 		$stdout = stream_get_contents($pipes[1]);
 		fclose($pipes[1]);
 		$stderr = stream_get_contents($pipes[2]);
--- includes/server/serverLibs.php
+++ includes/server/serverLibs.php
@@ -1,6 +1,4 @@
 <?
-define('PHP_UNIT',true);//true o false
-
 require_once SKEL_ROOT_DIR."includes/server/lib/misc.php";//biblioteca de funciones varias
 require_once SKEL_ROOT_DIR."includes/server/lib/returnInfo.php";//biblioteca de funciones para tratar $_SESSION['returnInfo']
 
@@ -17,10 +15,6 @@ require_once SKEL_ROOT_DIR."includes/server/vendor/PHPMailer_v5.1/class.phpmaile
 require_once SKEL_ROOT_DIR."includes/server/vendor/blueimp-jQuery-File-Upload/UploadHandler.php";
 require_once SKEL_ROOT_DIR."includes/server/vendor/jsmin-1.1.1.php";
 
-//CronExpression: https://github.com/mtdowling/cron-expression
-require_once SKEL_ROOT_DIR."includes/server/vendor/cron-expression-1.0.3/src/Cron/FieldInterface.php";
-foreach (glob(SKEL_ROOT_DIR."includes/server/vendor/cron-expression-1.0.3/src/Cron/*.php") as $filename) {require_once $filename;}
-
 //PHP Token Reflection: https://github.com/Andrewsville/PHP-Token-Reflection
 set_include_path(
 	realpath(__DIR__ . '/vendor/PHP-Token-Reflection-1.4.0') . PATH_SEPARATOR . // Library
@@ -33,30 +27,5 @@ spl_autoload_register(function($className) {
 		@include_once $file;
 	}
 });
-//PHPUnit: https://phpunit.de/  & https://github.com/sebastianbergmann/phpunit
-if (PHP_UNIT) {
-	$fileList=Filesystem::pharSearch (SKEL_ROOT_DIR.'includes/server/vendor/phpunit.phar',"/.*\.php/");
-	spl_autoload_register(function($className) use (&$fileList) {
-		//static $sg='';
-		//static $sgLen=2;
-		//$sg.='  ';
-		//error_log($sg."Intentando autoload: ".$className);
-		$className = @end(explode("\\",$className));//namespaces, genera: PHP Strict Standards:  Only variables should be passed by reference, pq end recibe el resultado de explode por referencia
-		$regEx='/.*(interface|class) '.$className.'\b/';
-		foreach ($fileList as $key => $path) {
-			$contents=file_get_contents($path);
-			if (preg_match($regEx, $contents)===1) {
-				//error_log($sg."+ ".$className." encontrada en: ".basename($path));
-				require_once ($path);
-				//unset ($fileList[$key]);
-				//error_log(count($fileList));
-				break;
-			}
-		}
-		//error_log($sg."Fin autoload: ".$className);
-		//$sg=substr($sg, 0,-$sgLen);
-	});
-}
-// Composer:
-require 'phar://'.realpath(SKEL_ROOT_DIR.'includes/server/vendor/composer.phar').'/vendor/autoload.php'; // require composer dependencies
+require SKEL_ROOT_DIR."includes/server/vendor/composerVendor/autoload.php";
 ?>
